c6f6f846dd9d879b73a05419139fe903b9de0f73
furage <71694701+furage@users.noreply.github.com>
2021-04-07 07:49:11 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,29 +1,36 @@
 (function(){
-    var e = document.createElement("script");
-    e.setAttribute("src","https://code.jquery.com/jquery-1.9.1.js");
-    document.body.append(e);
-})();
-jQuery.noConflict();
-(function($) {
-    $.getScript("https://furage.github.io/autoMatome/mylib/adblock.js");
-    $(document).on('ready', function() {
+    (function(){
+        var e = document.createElement("script");
+        e.setAttribute("src","https://code.jquery.com/jquery-1.9.1.js");
+        document.body.append(e);
+    })();
+    jQuery.noConflict();
+    (function($) {
+        $.getScript("https://furage.github.io/autoMatome/mylib/adblock.js");
+        $.getScript("https://yaju1919.github.io/lib/lib/yaju1919.js", function(){
+            $(document).on('ready', function(){ main($) });
+        });
+    })(jQuery);
+    function main($){
         $(".listWithImage,.article-footer,.article-sub-category").remove();
         $("#article-contents").parent().children().each(function(i,e){
             if(i) $(e).remove();
         });
         var h = $("<div>").appendTo("#chat");
-        function add(tag, ttl, str, def){
-            return $("<" + tag + ">").appendTo(
-                $("<div>").appendTo(h).text(ttl + " : ")
-            ).attr({
-                placeholder: str
-            }).val(def);
-        }
-        var inputName = add("input", "名前", "きょーた", $("#comment-form-author").val() || $("#author").val());
-        var inputText = add("textarea", "本文", "Shift+Enterで投稿").css({
+        var inputName = yaju1919.addInputText(h,{
+            id: "inputName",
+            title: "名前",
+            placeholder: "きょーた",
+            value: $("#comment-form-author").val() || $("#author").val(),
+        });
+        var inputText = yaju1919.addInputText(h,{
+            textarea: true,
+            id: "inputText",
+            title: "本文",
+            placeholder: "Shift+Enterで投稿",
             width: "80%",
-            height: 100
-        }).on("keypress", function(e){
+        });
+        $("#inputText").on("keypress", function(e){
             if(e.key === "Enter" && e.shiftKey) send();
         });
         h.append("<br>");
@@ -34,10 +41,10 @@ jQuery.noConflict();
             height: 50
         });
         function send(){
-            btn.add(inputName).add(inputText).attr("disabled", true);
+            btn.add("#inputName,#inputText").attr("disabled", true);
             $.post("https://comment.blogcms.jp/livedoor/furage/8461595/post",{
-                author: inputName.val(),
-                body: inputText.val(),
+                author: inputName(),
+                body: inputText(),
                 rating: "",
                 rating_icon: "star",
                 cookie: "on"
@@ -49,5 +56,5 @@ jQuery.noConflict();
                 location.reload();
             });
         }
-    });
-})(jQuery);
+    }
+})();
