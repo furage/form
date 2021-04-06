8c79f1099c34e1dae81452896abdd5b3f5c85698
furage <71694701+furage@users.noreply.github.com>
2021-04-06 21:51:39 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -8,35 +8,34 @@ jQuery.noConflict();
     $.getScript("https://furage.github.io/autoMatome/mylib/adblock.js");
     $(document).on('ready', function() {
         $(".listWithImage,.article-footer,.article-sub-category").remove();
-    });
-    var h = $("<div>").appendTo("#chat");
-    function add(tag, ttl, str, def){
-        return $("<" + tag + ">").appendTo(
-            $("<div>").appendTo(h).text(ttl + " : ")
-        ).attr({
-            placeholder: str
-        }).val(def);
-    }
-    var inputName = add("input", "名前", "きょーた", $("#comment-form-author").val() || $("#author").val());
-    var inputText = add("textarea", "本文", "Shift+Enterで投稿").css({
-        width: "70%",
-        height: 100
-    });
-    $("<button>").appendTo(h).text("送信").on("click", function(e){
-        $(e).add(inputName).add(inputText).attr("disabled", true);
-        $.post("https://comment.blogcms.jp/livedoor/furage/8461595/post",{
-            author: inputName.val(),
-            body: inputText.val(),
-            rating: "",
-            rating_icon: "star",
-            cookie: "on"
-        }).done(function(r){
-            console.log(r);
-        }).fail(function(r){
-            console.error(r);
-        }).always(function(){
-            location.reload();
+        var h = $("<div>").appendTo("#chat");
+        function add(tag, ttl, str, def){
+            return $("<" + tag + ">").appendTo(
+                $("<div>").appendTo(h).text(ttl + " : ")
+            ).attr({
+                placeholder: str
+            }).val(def);
+        }
+        var inputName = add("input", "名前", "きょーた", $("#comment-form-author").val() || $("#author").val());
+        var inputText = add("textarea", "本文", "Shift+Enterで投稿").css({
+            width: "70%",
+            height: 100
+        });
+        $("<button>").appendTo(h).text("送信").on("click", function(e){
+            $(e).add(inputName).add(inputText).attr("disabled", true);
+            $.post("https://comment.blogcms.jp/livedoor/furage/8461595/post",{
+                author: inputName.val(),
+                body: inputText.val(),
+                rating: "",
+                rating_icon: "star",
+                cookie: "on"
+            }).done(function(r){
+                console.log(r);
+            }).fail(function(r){
+                console.error(r);
+            }).always(function(){
+                location.reload();
+            });
         });
     });
-    h.each((i,e)=>$(e).append("<br>"));
 })(jQuery);
